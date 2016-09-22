b8cb74d6b981af17c3d043f6f4b9d46fd93ce85d
Huai-Che Lu <hc.acsalu@gmail.com>
2016-09-22 19:37:55 -0400

    Create project

---
+++ PAM/PAM.h
@@ -0,0 +1,19 @@
+//
+//  PAM.h
+//  PAM
+//
+//  Created by Huai-Che Lu on 9/22/16.
+//  Copyright © 2016 Cornell Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PAM.
+FOUNDATION_EXPORT double PAMVersionNumber;
+
+//! Project version string for PAM.
+FOUNDATION_EXPORT const unsigned char PAMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PAM/PublicHeader.h>
+
+
