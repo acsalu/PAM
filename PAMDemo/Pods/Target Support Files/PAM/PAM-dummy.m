08e3823de71664daacb9faa1188adcb0843a9fe6
Huai-Che Lu <hc.acsalu@gmail.com>
2016-10-08 12:20:43 -0400

    Add demo app

---
+++ PAMDemo/Pods/Target Support Files/PAM/PAM-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PAM : NSObject
+@end
+@implementation PodsDummy_PAM
+@end
---
+++ PAMDemo/Pods/Target Support Files/PAM/PAM-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double PAMVersionNumber;
+FOUNDATION_EXPORT const unsigned char PAMVersionString[];
+
